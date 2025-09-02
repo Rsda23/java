bec2b7f1ba9ccad5c438384c6ea9035ab30b9e7f
Rsda23 <rsda23.0@gmail.com>
2025-09-02 13:51:49 +0200

    feat: implement fizz buzz

--- src/main/java/com/isitech/bibliotheque/FizzBuzz.java
+++ src/main/java/com/isitech/bibliotheque/FizzBuzz.java
@@ -8,6 +8,9 @@ public class FizzBuzz {
     }
 
     public static String fizzBuzz(int n) {
+        if (n % 15 == 0) return "FizzBuzz";
+        if (n % 3 == 0) return "Fizz";
+        if (n % 5 == 0) return "Buzz";
         return "";
     }
 }
